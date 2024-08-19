e090f6e4eb37384d516cfb24684a30f939283744
BlackAssasine12 <hessel-jx@outlook.de>
2024-08-19 09:32:24 +0200

    Kostenloses Steigern

--- charbogen/filereader.js
+++ charbogen/filereader.js
@@ -11,6 +11,7 @@ document.getElementById('fileInput').addEventListener('change', function (event)
             genCharInfo(data);
             bindHideButtons();
             updateCharakterCalculation();
+            
         };
         reader.readAsText(file);
     }
@@ -149,27 +150,63 @@ function addInputChangeListeners() {
     inputElements.forEach(input => {
         input.addEventListener('change', updateCharakterCalculation);
     });
-    const mainInput = document.getElementById('erfahrung_Gesteigerte');
 
-    console.log(adjustments)
+    const mainInput = document.getElementById('erfahrung_Gesteigerte');
 
     Object.keys(adjustments).forEach(klass => {
         document.querySelectorAll(`.${klass}`).forEach(input => {
-            input.setAttribute('data-initial', input.value);
+            input.setAttribute('data-initial', parseFloat(input.value) || 0);
 
-            input.addEventListener('input', function () {
-                const initialValue = parseInt(input.getAttribute('data-initial'));
-                const newValue = parseInt(input.value);
-                const diff = newValue - initialValue;
-
-                if (!isNaN(diff)) {
-                    mainInput.value = parseInt(mainInput.value) + (diff * adjustments[klass]);
-                    input.setAttribute('data-initial', newValue);
-                }
-            });
+            input.addEventListener('input', handleInputChange);
         });
     });
 }
+
+function removeInputChangeListeners() {
+    const inputElements = document.querySelectorAll('.stg');
+    inputElements.forEach(input => {
+        input.removeEventListener('change', updateCharakterCalculation);
+    });
+
+    Object.keys(adjustments).forEach(klass => {
+        document.querySelectorAll(`.${klass}`).forEach(input => {
+            input.removeEventListener('input', handleInputChange);
+        });
+    });
+}
+
+function handleInputChange(event) {
+    const input = event.target;
+    const mainInput = document.getElementById('erfahrung_Gesteigerte');
+    
+    const initialValue = parseFloat(input.getAttribute('data-initial')) || 0;
+    const newValue = parseFloat(input.value) || 0;
+    const diff = newValue - initialValue;
+
+    if (!isNaN(diff)) {
+        // Holen des Klassennamens aus den Klassen des Input-Elements
+        const classList = input.classList;
+        let adjustmentValue = 1; // Standardwert falls keine Klasse gefunden wird
+
+        // Überprüfen jeder Klasse des Elements, ob es im adjustments-Objekt existiert
+        classList.forEach(cls => {
+            if (adjustments[cls] !== undefined) {
+                adjustmentValue = parseFloat(adjustments[cls]) || 1;
+            }
+        });
+
+        mainInput.value = parseFloat(mainInput.value) + (diff * adjustmentValue);
+        input.setAttribute('data-initial', newValue);
+    }
+}
+
+document.getElementById('toggleListenersCheckbox').addEventListener('change', function() {
+    if (!this.checked) {
+        addInputChangeListeners();
+    } else {
+        removeInputChangeListeners();
+    }
+});
 //NOTE - Nimmt die daten vom Filereader und erstellt den Charakterbogen mithilfe von createSection()
 function generateCharakterAttributes(data) {
     const walletContainer = document.getElementById('WalletContainer');
